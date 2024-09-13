d409448fa0e1b4a0cff2db1bec2c22308577d42b
LeandroRJ81 <leandrogt22@hotmail.com>
2024-09-13 14:50:37 -0300

    feat: exercise3_js

---
+++ Javascript/exercicio3/switch.js
@@ -0,0 +1,17 @@
+const resultadoNumerico = parseFloat(prompt(
+    "Escolha uma alternativa:\n a)\n b) \n c)")
+    )
+    
+    switch (resultadoNumerico) {
+        case 1:
+            alert("O resultado é 'a'")
+            break
+        case 2:
+            alert("O resultado é 'b'")
+            break
+        case 3:
+            alert("O resultado é 'c'")
+            break
+        default:
+            alert("Finalizando...")
+    }
